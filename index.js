fe821d10248453286aa8ef4850c9ebfd71c6b89e
Loïc Goyet <loic.goyet@gmail.com>
2020-05-25 23:24:00 +0200

    Implement proptypes

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 const HelloMessage = ({ name }) => {
   return <div>Hello {name}</div>;
+};
+
+HelloMessage.propTypes = {
+  name: PropTypes.string.isRequired,
 };
 
 const mountNode = document.getElementById("app");
