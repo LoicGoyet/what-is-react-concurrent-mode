b83bc1f95529796107f17ce2cbf618a4f204c5fa
Loïc Goyet <loic.goyet@gmail.com>
2020-05-25 23:21:04 +0200

    Implement format tools

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const HelloMessage = ({name}) => {
-    return (
-        <div>Hello {name}</div>
-    )
-}
+const HelloMessage = ({ name }) => {
+  return <div>Hello {name}</div>;
+};
 
 const mountNode = document.getElementById("app");
-ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
+ReactDOM.render(<HelloMessage name="Loïc" />, mountNode);
