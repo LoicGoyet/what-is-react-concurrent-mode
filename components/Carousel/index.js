62f0a46eabe791174d41808efcbb1599859978b8
Loïc Goyet <loic.goyet@gmail.com>
2020-05-28 23:32:19 +0200

    use lazy loading on components and suspense

--- components/Carousel/index.js
+++ components/Carousel/index.js
@@ -1,28 +1,48 @@
 import React from "react";
 
 import "./style.scss";
-import BigThunderMountainThumbnail from "../BigThunderMountainThumbnail";
-import PhantomManorThumbnail from "../PhantomManorThumbnail";
-import SpaceMountainThumbnail from "../SpaceMountainThumbnail";
+const BigThunderMountainThumbnail = React.lazy(() =>
+  import("../BigThunderMountainThumbnail")
+);
+const PhantomManorThumbnail = React.lazy(() =>
+  import("../PhantomManorThumbnail")
+);
+const SpaceMountainThumbnail = React.lazy(() =>
+  import("../SpaceMountainThumbnail")
+);
 
 const Carousel = () => {
   const [index, setIndex] = React.useState(0);
 
+  const [startTransition, isPending] = React.unstable_useTransition({
+    timeoutMs: 500,
+  });
+
   const handleNextClick = (e) => {
     e.preventDefault();
-    if (index === 2) return setIndex(0);
-    return setIndex(index + 1);
+    startTransition(() => {
+      if (index === 2) return setIndex(0);
+      return setIndex(index + 1);
+    });
   };
 
   return (
     <div className="carousel">
-      {index === 0 && <BigThunderMountainThumbnail />}
-      {index === 1 && <PhantomManorThumbnail />}
-      {index === 2 && <SpaceMountainThumbnail />}
+      <React.Suspense
+        fallback={<div className="carousel__loading">loading</div>}
+      >
+        {index === 0 && <BigThunderMountainThumbnail />}
+        {index === 1 && <PhantomManorThumbnail />}
+        {index === 2 && <SpaceMountainThumbnail />}
 
-      <button className="carousel__btn" onClick={handleNextClick}>
-        &#x27E9;
-      </button>
+        <button
+          className="carousel__btn"
+          onClick={handleNextClick}
+          disabled={isPending}
+        >
+          &#x27E9;
+        </button>
+      </React.Suspense>
     </div>
   );
 };
