780f32d598c54736663b1e96c3c367a151b2d3c7
Loïc Goyet <loic.goyet@gensdeconfiance.fr>
2020-11-18 21:22:56 +0100

    Decrease image size

--- components/ImageGrid/index.js
+++ components/ImageGrid/index.js
@@ -7,32 +7,32 @@ const images = [
   {
     id: "moutains",
     src:
-      "https://images.unsplash.com/photo-1590670908865-c3330d76a188?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1273&q=80",
+      "https://images.unsplash.com/photo-1590670908865-c3330d76a188?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=210&q=80",
   },
   {
     id: "city",
     src:
-      "https://images.unsplash.com/photo-1590677481249-19b1afb5af8d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80",
+      "https://images.unsplash.com/photo-1590677481249-19b1afb5af8d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=230&q=80",
   },
   {
     id: "space",
     src:
-      "https://images.unsplash.com/photo-1590673051864-3520f4c145bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1298&q=80",
+      "https://images.unsplash.com/photo-1590673051864-3520f4c145bc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=220&q=80",
   },
   {
     id: "cascade",
     src:
-      "https://images.unsplash.com/photo-1590664114966-20cd1ce7e09d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
+      "https://images.unsplash.com/photo-1590664114966-20cd1ce7e09d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80",
   },
   {
     id: "liquid",
     src:
-      "https://images.unsplash.com/photo-1590635355104-9c583abd5d82?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2552&q=80",
+      "https://images.unsplash.com/photo-1590635355104-9c583abd5d82?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=210&q=80",
   },
   {
     id: "nurse",
     src:
-      "https://images.unsplash.com/photo-1590611936760-eeb9bc598548?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
+      "https://images.unsplash.com/photo-1590611936760-eeb9bc598548?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=210&q=80",
   },
 ];
 
